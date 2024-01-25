b2dd5150613a583c4a5b1fcb029fe8fc2cddc395
Dawit Mellese <dawitm777@gmail.com>
2024-01-25 17:03:00 +0300

    adding templates commit

---
+++ playground/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('hello/', views.say_hello),
+    path('', views.say_test)
+]
+
+
--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def say_hello(request):
+    return render(request, 'hello.html', context={'name':"dawit"})
+
+
+def say_test(request):
+    return HttpResponse('Hello test')
+
+
+
